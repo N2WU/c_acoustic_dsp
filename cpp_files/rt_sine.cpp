aee974def3c16d3e6aaf61095b5d618f4b78ead0
nolan <pearcenolan@gmail.com>
2025-06-12 17:34:54 -0400

    change text

--- cpp_files/rt_sine.cpp
+++ cpp_files/rt_sine.cpp
@@ -5,26 +5,19 @@
 #include <math.h>
 #define SLEEP( milliseconds ) usleep( (unsigned long) (milliseconds * 1000.0) )
  
-// Two-channel sawtooth wave generator.
+// Single-Channel Sine Wave generator.
 int saw( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
          double streamTime, RtAudioStreamStatus status, void *userData )
 {
-  unsigned int i; //, j;
+  unsigned int i;
   double *buffer = (double *) outputBuffer;
-  //double *lastValues = (double *) userData;
  
   if ( status )
     std::cout << "Stream underflow detected!" << std::endl;
  
   // Write interleaved audio data.
   for ( i=0; i<nBufferFrames; i++ ) {
-    //for ( j=0; j<1; j++ ) {
-      //*buffer++ = lastValues[j];
- 
-      //lastValues[j] += 0.005 * (j+1+(j*0.1));
-      buffer[i] = 16383.0 * sin(i*3000.0*2.0*M_PI/44100);
-      //if ( lastValues[j] >= 1.0 ) lastValues[j] -= 2.0;
-    //}
+    buffer[i] = 16383.0 * sin(i*3000.0*2.0*M_PI/44100);
   }
  
   return 0;
@@ -45,7 +38,7 @@ int main()
   parameters.firstChannel = 0;
   unsigned int sampleRate = 44100;
   unsigned int bufferFrames = 44100; // 256 sample frames
-  double data[1] = {0}; // data[2] = {0,0};-> buffer?
+  double data[1] = {0};
  
   if ( dac.openStream( &parameters, NULL, RTAUDIO_FLOAT64, sampleRate,
                        &bufferFrames, &saw, (void *)&data ) ) {
@@ -58,14 +51,9 @@ int main()
     std::cout << dac.getErrorText() << std::endl;
     goto cleanup;
   }
-  //std::cout << "\nPlaying ... \n";
-  //char input;
   SLEEP (100); // sleep 
-  //std::cin.get( input );
   std::cout << "\nStopping ... \n";
   // Block released ... stop the stream
-  //if ( dac.isStreamRunning() )
-  //  dac.stopStream();  // or could call dac.abortStream();
   dac.abortStream();
 
  cleanup:
