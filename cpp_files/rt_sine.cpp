c2f7cf7405bf6b18322e8b10d109ea61fe6360cc
nolan <pearcenolan@gmail.com>
2025-06-12 11:50:37 -0400

    Add Sine Example

--- cpp_files/rt_sine.cpp
+++ cpp_files/rt_sine.cpp
@@ -1,27 +1,29 @@
-#define _USE_MATH_DEFINES
-
-#include <iostream>
 #include "RtAudio.h"
-#include <cmath>
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <math.h>
+#define SLEEP( milliseconds ) usleep( (unsigned long) (milliseconds * 1000.0) )
  
 // Two-channel sawtooth wave generator.
 int saw( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
          double streamTime, RtAudioStreamStatus status, void *userData )
 {
-  unsigned int i;
+  unsigned int i; //, j;
   double *buffer = (double *) outputBuffer;
-  double *lastValues = (double *) userData;
+  //double *lastValues = (double *) userData;
  
   if ( status )
     std::cout << "Stream underflow detected!" << std::endl;
  
   // Write interleaved audio data.
   for ( i=0; i<nBufferFrames; i++ ) {
-    //for ( j=0; j<2; j++ ) {
-      *buffer++ = lastValues[i];
+    //for ( j=0; j<1; j++ ) {
+      //*buffer++ = lastValues[j];
  
-      lastValues[i] = 6383.0 * sin(i*3000.0*2.0*M_PI/44100);
-    //  if ( lastValues[j] >= 1.0 ) lastValues[j] -= 2.0;
+      //lastValues[j] += 0.005 * (j+1+(j*0.1));
+      buffer[i] = 16383.0 * sin(i*3000.0*2.0*M_PI/44100);
+      //if ( lastValues[j] >= 1.0 ) lastValues[j] -= 2.0;
     //}
   }
  
@@ -36,14 +38,14 @@ int main()
     std::cout << "\nNo audio devices found!\n";
     exit( 0 );
   }
- 
+  std::cout << "\nPlaying ... \n";
   RtAudio::StreamParameters parameters;
   parameters.deviceId = dac.getDefaultOutputDevice();
-  parameters.nChannels = 2;
+  parameters.nChannels = 1; //2
   parameters.firstChannel = 0;
   unsigned int sampleRate = 44100;
-  unsigned int bufferFrames = 256; // 256 sample frames
-  double data[2] = {0, 0};
+  unsigned int bufferFrames = 44100; // 256 sample frames
+  double data[1] = {0}; // data[2] = {0,0};-> buffer?
  
   if ( dac.openStream( &parameters, NULL, RTAUDIO_FLOAT64, sampleRate,
                        &bufferFrames, &saw, (void *)&data ) ) {
@@ -56,15 +58,16 @@ int main()
     std::cout << dac.getErrorText() << std::endl;
     goto cleanup;
   }
-  
-  char input;
-  std::cout << "\nPlaying ... press <enter> to quit.\n";
-  std::cin.get( input );
- 
+  //std::cout << "\nPlaying ... \n";
+  //char input;
+  SLEEP (100); // sleep 
+  //std::cin.get( input );
+  std::cout << "\nStopping ... \n";
   // Block released ... stop the stream
-  if ( dac.isStreamRunning() )
-    dac.stopStream();  // or could call dac.abortStream();
- 
+  //if ( dac.isStreamRunning() )
+  //  dac.stopStream();  // or could call dac.abortStream();
+  dac.abortStream();
+
  cleanup:
   if ( dac.isStreamOpen() ) dac.closeStream();
  
